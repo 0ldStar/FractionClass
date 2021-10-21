de4993aca7c20f164478c27e2b6981ef6b0ad1bd
0ldStar <brosto_man@mail.ru>
2021-10-21 08:51:23 +0700

    fixed

--- Google_tests/test.cpp
+++ Google_tests/test.cpp
@@ -3,8 +3,6 @@
 //
 #include "gtest/gtest.h"
 #include "../Fraction.h"
-#include "../CompleteFracion.h"
-#include "../MixedFractions.h"
 #include <iostream>
 
 #include <fstream>
@@ -97,67 +95,3 @@ TEST(lab3, fileSteamTest) {
     ASSERT_TRUE(!strcmp(b.getStr(), c.getStr()));
     binOut.close();
 }
-
-
-TEST(lab4, CompleteFractionsConstructorTests) {
-
-    CompleteFracion a(11, 33);
-    CompleteFracion b(-1, 4);
-    CompleteFracion c(1, 2);
-    CompleteFracion d;
-
-    ASSERT_THROW(CompleteFracion e(1, 0), exception);
-    ASSERT_STREQ(a.getStr(), "1/3");
-    ASSERT_STREQ(b.getStr(), "-1/4");
-    ASSERT_STREQ(c.getStr(), "1/2");
-
-    d = a;
-    a = b;
-    ASSERT_STREQ(d.getStr(), "1/3");
-}
-
-
-TEST(lab4, CompleteFractionsResultTest) {
-    CompleteFracion a(1, 2);
-    CompleteFracion b(1, 3);
-
-    ASSERT_EQ(a.getRes(), 0.5);
-    ASSERT_DOUBLE_EQ(b.getRes(), (double) 1 / 3);
-}
-
-TEST(lab4, MixedFractionsConstructorTests) {
-
-    MixedFractions a(1, 11, 33);
-    MixedFractions b(2, -1, 4);
-    MixedFractions c(-3, 1, 2);
-    MixedFractions d;
-
-    ASSERT_THROW(MixedFractions e(1, 1, 0), exception);
-    ASSERT_STREQ(a.getStr(), "1|1/3");
-    ASSERT_STREQ(b.getStr(), "2|-1/4");
-    ASSERT_STREQ(c.getStr(), "-3|1/2");
-    d = a;
-    a = b;
-    ASSERT_TRUE(!strcmp("1|1/3", d.getStr()));
-}
-
-
-TEST(lab4, MixedFractionsArifmeticOperationsTests) {
-    MixedFractions a;
-    MixedFractions b(2, -1, 4);
-    MixedFractions c(-3, 1, 2);
-    MixedFractions d(-3, 0, 2);
-
-
-    a = b / c;
-    ASSERT_STREQ(a.getStr(), "|-7/10");
-    a = b * c;
-    ASSERT_STREQ(a.getStr(), "-4|3/8");
-    a = b + c;
-    ASSERT_STREQ(a.getStr(), "|-3/4");
-    a = b - c;
-    ASSERT_STREQ(a.getStr(), "4|1/4");
-
-    ASSERT_THROW(a / d, exception);
-}
-
